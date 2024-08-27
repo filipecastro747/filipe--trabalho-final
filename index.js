7bf716f0d3c144604b5718f15ce9991ad2ded92f
filipecastro747 <castrofilipe449@gmail.com>
2024-08-27 16:30:32 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,234 @@
+
+//let qualsuaidade=prompt("qual sua idade")
+//let idademelhoramigo=prompt("idade melhor amigo")
+//console.log("sua idade é maior ou menor que a do seu melhor amigo?"+ qualsuaidade > idademelhoramigo)
+//console.log (qualsuaidade - idademelhoramigo)
+//let numeropar=prompt(" escreva um numero par")
+//const restoDaDivisão =  numeropar % 2 
+//console.log(restoDaDivisão) 
+//let nome=prompt( "insira seu nome " )
+//let cor=prompt(" insira sua cor favorita" )
+//const informacoes= " a cor favorita de " + nome + " é a cor " + cor
+//console.log (informacoes)
+//console.log ( ` nome ${nome} a cor favorita de  ${cor} `)
+//let frase=prompt("escrever uma frase")
+////console.log(frase.toUpperCase())
+//console.log (frase.replaceAll ("i" , "o") )
+//console.log(frase.length)
+//const racaDeCachorro =["Buldogue","pastor alemão","são bernardo","Chihuahua","Husky siberiano"] 
+//let racas= racaDeCachorro[Number (prompt("digite uma numero de 0 a 4"))]
+//console.log(racas)
+//let numeros =[1,2,3,4,5,6]
+//console.log(numeros.length)
+//numeros.push(7)
+//console.log(numeros)
+//numeros.splice(3, 2) 
+//console.log(numeros)
+//console.log(numeros.length)
+//let nomeDoUsuario = prompt("nome do usuario")
+//let emailDoUsuario = prompt("email do usuario")
+//console.log("o e-mail" + emailDoUsuario + "foi cadastrado com sucesso seja(o) bem vindo!" + nomeDoUsuario)
+//const comiDaSpreferidas = ["hamburger","batata", "alface","chocolate", "polvo"]
+//console.log(comiDaSpreferidas)
+//console.log("essas são minhas comidas preferidas" + comiDaSpreferidas ) 
+//const pegunteaousuario = prompt("qual sua comida preferida ?") 
+//comiDaSpreferidas [1] = pegunteaousuario
+//console.log(comiDaSpreferidas [0])
+//console.log(comiDaSpreferidas [1])
+//console.log(comiDaSpreferidas [2])
+//console.log(comiDaSpreferidas [3])
+//console.log(comiDaSpreferidas [4])
+//console.log(comiDaSpreferidas)
+//let array = [] 
+//let talistaDeTarefas = array
+//let tarefaUM = prompt("1 tarefa que voce realiza no dia a dia ")
+//let tarefaDOIS = prompt("  1  tarefas que voce realiza no dia a dia ")
+//let tarefaTRES = prompt("  1  tarefas que voce realiza no dia a dia ")
+//array [1] = tarefaUM
+//array [2] = tarefaDOIS
+//array [3] = tarefaTRES
+//console.log(talistaDeTarefas)
+//let indice = Number prompt (digi 
+//function imprimirOlaMundo() {
+    //console.log("ola mundo")
+//} 
+//imprimirOlaMundo()
+
+//let nome = function (nome) { 
+    //console.log(`olá ${nome}`)
+//}
+//nome()
+  //function nomeDOIS(nome1, nome2, nome3) {
+    //console.log(` ola ${nome1}, ${nome2} e ${nome3}`)
+   //}
+    //nome("caio", "Kauã", "Victor") 
+
+    //let soma =  (numero1, numero2) => { 
+    //return numero1 + numero2
+    //}
+    //let b=soma(3,4) 
+    //console.log(b) 
+    
+//const onepiece= {
+ // nome:'one piece egghead',
+  //ano: '2023',
+  //elenco :['carol','glauco','Wendel'],
+  //viuOuNao: true
+//} 
+//console.log(onepiece.nome)
+//console.log(onepiece.ano)
+//console.log(onepiece["elenco"])
+//console.log(onepiece["viuOuNao"])
+
+//const pessoa= {
+// nome:'filipe',
+ //idade: '15',
+ //GeneroMusical:'anime'
+
+//}
+//console.log( "o nome da pessoa é" + pessoa.nome + " ele tem " + pessoa.idade + " anos e gosta muito de " + pessoa.GeneroMusical)
+
+//onepiece.listaDePersonagens = ['nami','robin','chooper']
+
+//console.log(onepiece.listaDePersonagens[0]+ " : " + onepiece. elenco[0])
+//onepiece.elenco[0]='xuxa'
+
+//console.log(onepiece.elenco)
+///console.log(onepiece.nome)
+//console.log(onepiece.ano)
+//console.log(onepiece['elenco'])
+//console.log(onepiece['viuOuNao'])
+//console.log(onepiece['listaDePersonagens'])
+
+//let a  = Number (prompt ("digite um numero ?") )
+
+//let  b = Number (prompt ("digite outro numero ? ") )
+
+ //function comparador (num1,num2) {
+//if (num1 === num2) {
+//console.log("sucesso")  
+//}
+
+//else { 
+//console.log("diferente")
+//}
+//}
+
+//comparador(a, b)
+
+
+//let a  = Number (prompt ("digite um numero ?") )
+
+//let  b = Number (prompt ("digite outro numero ? ") )
+
+ //function comparador (num1,num2) {
+//if (num1 === num2) {
+//console.log("sucesso")  
+//}
+
+ //if  (num1===num2) {
+//console.log("igual")
+ //}else if (num1>num2)
+// console.log("maior")
+
+//console.log("menor")
+//}
+ //comparador(a,b)
+
+ //let pokemon = prompt("digete um pokemon")
+ //switch (pokemon) {
+  //case 'Bulbasauro':
+    //console.log('planta e veveno')
+   // break
+ // case  'Charmander':
+ //console.log('fogo')
+    //break
+  //case  ' Squirtle ':
+    //console.log('água')
+    //break
+
+  //default:
+ //console.log("O tipo não foi encontrado") 
+ //break
+ //}
+
+ //let ensino = prompt(" se tiver concluido o ensino medio" )
+ //let idade = Number (prompt("tiver 18 anos ou mais "))
+ //let cursando = prompt("não estiver cursando outra faculdade")
+ 
+ //function medio (ensino,idade,cursando){
+  //if (idade <= 18 && cursando === "sim" && cursando === "não"){
+    //console.log("você pode estudar na faculdade")
+  
+  //}
+  //else {
+   // console.log('você pode não estudar na faculdade')
+
+   
+// }
+//}
+//medio(idade, ensino,cursando)
+
+//function sonic() {
+  //let soma=0
+ // while(true){
+    //let numero= number (prompt("digite um numero ou 0 para sair"))
+    //if(numero===0)
+    //break
+  //}
+//}
+//soma=numero+soma
+//console.log(soma)
+
+//(sonic)
+
+//let numeros =  [10,20,30,40,50]
+//for(let numeros =  
+
+
+ let menu= Number = prompt ("===== ABRIGOS  TEMPORÁRIOS =====\n 1 cadastre abrigo\n 2 Listar abrigos \n 3 Procurar abrigo \n 4 Sair")
+
+ switch (menu){
+  case"1" : 
+  prompt( "qual sera o nome do abrigo?")
+  break
+  
+  case "2":
+  prompt("qual sera o endereço do abrigo")  
+break
+
+case "3":
+ prompt("qual sera o telefone do abrigo ")
+break
+
+case "4" :
+  prompt("qual é a capacidade maxima de pessoas no abrigo")
+break
+
+
+ }
+   
+const cadastro={
+  nomeDoAbrigo: menu,
+  endereço: menu,
+  teelfone: menu,
+  capacidadeDoAbrigo: menu,
+}
+
+
+let  abrigo= [
+  {nomeDoAbrigo: 'abrigo', modolo: 1 },
+  {endereço: 'abrigo', modolo: 2},
+  {teelfone: 'abrigo', modolo: 3},
+  {capacidadeDoAbrigo: 'abrigo', modolo: 4 },
+]
+console.log(menu [0])
+
+
+   
+
+
+
+
+
+
